af5fafb9e61bc1eda29b0c17058acbbbe45cd0e9
MAJU STUDENT <faizaramsha01@gmail.com>
2019-05-05 18:50:03 +0500

    5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { View } from "react-native";
-//Redux specific
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-//Get Reducers
 import reducers from "./reducers";
-//Get the Header Component
 import { Header } from "./components/common";
-// import LibraryList from "./components/LibraryList";
+import LibraryList from "./components/LibraryList";
 
 const App = () => {
-return (
-// Provider can only have one child component
-<Provider store={createStore(reducers)}>
-<View>
-<Header headerText="Redux Demo -- Tech Stack" />
-{/* <LibraryList /> */}
-</View>
-</Provider>
-);
-};
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+  };
 
 export default App;
